9c89f2fd3cc219a21b7500d70530b8f75595a88b
wenying823 <35444845+wenying823@users.noreply.github.com>
2022-01-19 14:14:29 +0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
 	$dbHost = "172.16.2.165";
 	$dbDatabase = "GameList";
-	$dbPasswrod = "ksmas@2015";
+	$dbPasswrod = "";
 	$dbUser = "stockmas";
 	$mysqli = new mysqli($dbHost, $dbUser, $dbPasswrod, $dbDatabase);
 ?>
